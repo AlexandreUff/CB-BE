3c6fbd4b31784f85ba66a6002e5c75ce1af2385c
AlexandreUff <alexandre_magno@id.uff.br>
2024-01-22 23:12:35 -0300

    fix: add CORS solution

--- index.js
+++ index.js
@@ -4,6 +4,19 @@ const port = 3000;
 
 const uploadRoutes = require('./routes/uploadRoutes')
 
+server.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', 'http://localhost:5173'); // Replace with your client-side origin
+  res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+
+  // Allow preflight requests to succeed
+  if (req.method === 'OPTIONS') {
+    res.sendStatus(200);
+  } else {
+    next();
+  }
+});
+
 server.get('/', (req, res) => {
   res.send('Hello, Worlds!');
 });
